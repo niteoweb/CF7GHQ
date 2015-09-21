c6dc82386a1a816808e3cadc094a569301c5954c
dz0ny <dz0ny@ubuntu.si>
2015-09-21 15:33:27 +0200

    Add tests

--- Makefile
+++ Makefile
@@ -2,3 +2,17 @@ VERSION := 1.0.0
 
 test:
 	bin/phpunit
+
+release:
+	cp -ar src cf7ghq
+	zip -r cf7ghq.zip cf7ghq
+	rm -rf cf7ghq
+	mv cf7ghq.zip build/
+
+deploy:
+	-bin/linux/amd64/github-release delete -u niteoweb -r cf7ghq -t v$(VERSION)
+	-bin/linux/amd64/github-release delete -u niteoweb -r cf7ghq -t latest
+	bin/linux/amd64/github-release release -u niteoweb -r cf7ghq -t v$(VERSION)
+	bin/linux/amd64/github-release release -u niteoweb -r cf7ghq -t latest
+	bin/linux/amd64/github-release upload -u niteoweb -r cf7ghq -t v$(VERSION) -f build/cf7ghq.zip -n cf7ghq.zip
+	bin/linux/amd64/github-release upload -u niteoweb -r cf7ghq -t latest -f build/cf7ghq.zip -n cf7ghq.zip
--- tests/TestPlugin.php
+++ tests/TestPlugin.php
@@ -1,25 +1,167 @@
 <?php
 use Niteoweb\WP7GHQ\CF7GHQ;
 
-class TestIntegration extends PHPUnit_Framework_TestCase {
+$WPCF7_Submission_mock = null;
 
-		function setUp() {
-			\WP_Mock::setUsePatchwork( true );
-			\WP_Mock::setUp();
-		}
+class WPCF7_Submission{
+    public static function get_instance( $contact_form = null ) {
+        global $WPCF7_Submission_mock;
+        return $WPCF7_Submission_mock;
+    }
+}
 
-		function tearDown() {
-			\WP_Mock::tearDown();
-		}
+class TestIntegration extends PHPUnit_Framework_TestCase
+{
+    use \Xpmock\TestCaseTrait;
 
-		public function test_init() {
-			$plugin = new CF7GHQ;
+    function setUp()
+    {
+        \WP_Mock::setUp();
+    }
 
-			\WP_Mock::expectActionAdded( 'wpcf7_submit', array( $plugin, 'submitForm' ) );
+    function tearDown()
+    {
+        global $WPCF7_Submission_mock;
+        \WP_Mock::tearDown();
+        $WPCF7_Submission_mock = null;
+    }
 
-			$plugin->__construct();
-			\WP_Mock::assertHooksAdded();
-		}
+    public function test_init()
+    {
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('pluginGCF7GHQ_settings'),
+                'return' => array('cf7ghq_prevent_email' => "1")
+            )
+        );
+        $plugin = new CF7GHQ;
+        $plugin->__construct();
+    }
+
+    public function test_init_default()
+    {
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('pluginGCF7GHQ_settings'),
+                'return' => array('cf7ghq_none' => true)
+            )
+        );
+        $plugin = new CF7GHQ;
+        $plugin->__construct();
+    }
+
+    public function test_submit_missing_cf()
+    {
+        global $WPCF7_Submission_mock;
+        $WPCF7_Submission_mock = $mock = $this->mock()
+            ->get_posted_data(false)
+            ->new();
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('pluginGCF7GHQ_settings'),
+                'return' => array('cf7ghq_none' => true)
+            )
+        );
+        $plugin = new CF7GHQ;
+        $mock = $mock = $this->mock()
+            ->in_demo_mode(false)
+            ->additional_setting(false)
+            ->new();
+        $plugin->submitForm($mock);
+    }
+
+    public function test_submit_demo_mode()
+    {
+        global $WPCF7_Submission_mock;
+        $WPCF7_Submission_mock = $mock = $this->mock()
+            ->get_posted_data(false)
+            ->new();
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('pluginGCF7GHQ_settings'),
+                'return' => array('cf7ghq_none' => true)
+            )
+        );
+        $plugin = new CF7GHQ;
+        $mock = $mock = $this->mock()
+            ->in_demo_mode(true)
+            ->additional_setting(false)
+            ->new();
+        $plugin->submitForm($mock);
+    }
+
+    public function test_submit_full()
+    {
+        global $WPCF7_Submission_mock;
+        $WPCF7_Submission_mock = $mock = $this->mock()
+            ->get_posted_data(array(
+                "your-email" => "user@test.tld",
+                "test-key" => "something"
+            ))
+            ->new();
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('pluginGCF7GHQ_settings'),
+                'return' => array('cf7ghq_none' => true)
+            )
+        );
+        \WP_Mock::wpFunction('wp_mail', array(
+                'called' => 1,
+                'args' => array(NULL, "Test: user@test.tld", "something"),
+            )
+        );
+        \WP_Mock::wpFunction('remove_filter', array(
+                'called' => 1,
+            )
+        );
+        $plugin = new CF7GHQ;
+        $mock = $mock = $this->mock()
+            ->in_demo_mode(false)
+            ->additional_setting(false)
+            ->title("Test")
+            ->prop("[input test-key something]")
+            ->new();
+        $plugin->submitForm($mock);
+    }
+
+    public function test_submit_full_admin_email()
+    {
+        global $WPCF7_Submission_mock;
+        $WPCF7_Submission_mock = $mock = $this->mock()
+            ->get_posted_data(array(
+                "test-key" => "something"
+            ))
+            ->new();
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('pluginGCF7GHQ_settings'),
+                'return' => array('cf7ghq_none' => true)
+            )
+        );
+        \WP_Mock::wpFunction('get_option', array(
+                'called' => 1,
+                'args' => array('admin_email'),
+                'return' => "admin@domain.tld"
+            )
+        );
+        \WP_Mock::wpFunction('wp_mail', array(
+                'called' => 1,
+                'args' => array(NULL, "Test: admin@domain.tld", "something"),
+            )
+        );
+        \WP_Mock::wpFunction('remove_filter', array(
+                'called' => 1,
+            )
+        );
+        $plugin = new CF7GHQ;
+        $mock = $mock = $this->mock()
+            ->in_demo_mode(false)
+            ->additional_setting(false)
+            ->title("Test")
+            ->prop("[input test-key something]")
+            ->new();
+        $plugin->submitForm($mock);
+    }
 
 
-	}
+}
